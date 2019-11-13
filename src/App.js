055cf26e7f356f7b337f99b1fee4b55f6e9d7867
Obiageli Ogbutor <obygirl81@gmail.com>
2019-11-12 19:57:21 -0700

    uploading the api routes files for tasks, users, and to view all sponsor incentives

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import AppCards from './components/AppCards/AppCards'
+import screen from "./screens.json";
+import Wrapper from './components/Wrapper/Wrapper'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let highPointCount = 0;
+
+class App extends Component {
+  state = {
+    screen, 
+    Point: 0, 
+    highPoint: 0,
+    status: "Click each screen view content!"
+  };
+
+  handleClickedScreen = id => {
+  
+    const screenPicked = this.state.screen.filter(screen => screen.id === id);
+    console.log(screenPicked);
+
+    if (this.state.Point < 11 && !screenPicked[0].chosenScreen) {
+      screenPicked[0].chosenScreen = true;
+      console.log(screenPicked); 
+      this.setState({ screen });
+      console.log(screen);
+      screen.sort(() => Math.random() - 0.5);
+      this.setState({ Point: this.state.Point + 1 });
+      this.setState({ status: "You're doing great!" });
+      if (this.state.highPoint <= this.state.Point) {
+        highPointCount++;
+        this.setState({ highPoint: highPointCount });
+      }
+    } else if (this.state.Point <= 11 && screenPicked[0].chosenScreen) {
+        this.setState({ status: "That wasn't a fresh screen! Try selecting a new screen again!" });
+        this.setState({ Point: 0 });
+        screen.forEach(screen => {
+          screen.chosenScreen = false; 
+        });
+        this.setState({ screen });
+        console.log(screen);
+    } else if (this.state.Point === 11 && !screenPicked[0].chosenScreen) {
+      this.setState({ status: "Congratulations! You have the highest Point!" });
+      this.setState({ highPoint: 12 });
+      this.setState({ Point: 0 });
+
+      screen.forEach(screen => {
+        screen.chosenScreen = false; 
+      });
+
+      this.setState({ screen });
+      console.log(screen);
+
+      screen.sort(() => Math.random() - 0.5);
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+          <nav className="nav-container">
+            <h1 className="main-header">Best Life!!!</h1>
+            <ul className="nav-elements">
+              <li className="each-nav-element">Points: {this.state.Point}</li>
+              <li className="each-nav-element">Highest Points: {this.state.highPoint}</li>
+              <li className="each-nav-element">{this.state.status}</li>
+            </ul>
+          </nav>
+        <Wrapper>
+          {this.state.screen.map(screen => (
+            <AppCards
+              handleClickedScreen={this.handleClickedScreen}
+              id={screen.id}
+              key={screen.id}
+              name={screen.name}
+              image={screen.image}
+            />
+          ))}
+        </Wrapper>  
+        <footer className="footer-container">
+          <p className="footer-content">
+              © 2019 <a className="github-link" href='#'>Mongolicious</a>
+          </p>
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
