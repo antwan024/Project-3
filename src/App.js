a2567228dd4f2af4a213cd7ce043defa0ee9dda3
daniel taquez <daniel.taquez@du.edu>
2019-11-14 19:20:50 -0700

    Newest branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
 import React, { Component } from "react";
->>>>>>> c6b304c5bb64fe19c5eb9d5480a9d1d45bd64d62
 import './App.css';
 import MenuExampleProps from  './components/NavBar/MenuExampleProps';
 import './components/Tiles/CardExampleGroups'
