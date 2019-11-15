a4206d74a17714fbb6e0ecd7596e576bad8a453a
Nina Shin <48607661+ninashin66@users.noreply.github.com>
2019-11-14 20:02:04 -0700

    added folder structure

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import MenuExampleProps from "./components/NavBar/MenuExampleProps";
+import "./components/Tiles/CardExampleGroups";
+import CardExampleGroups from "./components/Tiles/CardExampleGroups";
+
+function App() {
+  return (
+    <div>
+      <MenuExampleProps />
+      <CardExampleGroups />
+    </div>
+  );
+}
+
+export default App;
